912f90a06760315ff4b227c6e5be7fce0eff87ae
Jaris <90911153+JohnARI@users.noreply.github.com>
2022-09-26 11:41:10 +0200

    auth

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -11,7 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class DashboardController extends AbstractController
 {
     /**
-     * @Route("admin/dashboard", name="app_dashboard")
+     * @Route("dashboard", name="app_dashboard")
      */
     public function index(): Response
     {
