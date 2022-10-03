cbbf16f1c4cd3d152f0861b32a54fb26e026eb9e
Jaris <90911153+JohnARI@users.noreply.github.com>
2022-10-03 16:16:21 +0200

    formaulaires

--- src/Form/JobTypesType.php
+++ src/Form/JobTypesType.php
@@ -6,14 +6,19 @@ use App\Entity\JobTypes;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class JobTypesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-        ;
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'mt-6 w-full border-b-2 border-violet-600 outline-0 h-10',
+                    'placeholder' => 'Title'
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
